0fd50fcbe3011ead858adeed00804b6e85041342
26space <26sp330@gmail.com>
2018-02-14 22:17:23 +0000

    counting...

--- src/js/classifiers/naivebayes.js
+++ src/js/classifiers/naivebayes.js
@@ -11,6 +11,16 @@ export class NaiveBayesClassifier {
     this.frameSize = 100;
 
     this.behaviors = ["walk", "crouch", "still"];
+    this.current_behavior_state = "still";
+
+    this.spike_begin = false;
+    this.actually_staying_still = false;
+    this.spike_start_time = 0;
+    this.last_spike_end_time = 0;
+    
+    this.steps2 = 0;
+    this.sensitivity_level = 0.75;
+    this.minimum_interval_steps = 1;
 
     this.priors = {
       walk: 0.4,
@@ -20,6 +30,7 @@ export class NaiveBayesClassifier {
 
 
     this.behaviorsCounts = {};
+    this.behaviorsDetailCounts = {};
 
     this.mean_mu_params = {
       walk: walk[0],
@@ -45,6 +56,39 @@ export class NaiveBayesClassifier {
       crouch: crouch[3]
     };
 
+    this.spike_threshold_params = {};
+    this.max_values = {
+      walk: walk[4],
+      still: still[4],
+      crouch: crouch[4]
+    };
+
+    for (var i = 0; i < this.behaviors.length; i++) {
+      var behav = this.behaviors[i]
+      var peak_threshold_values = [];
+      var threshold_value_ax = this.mean_mu_params[behav].AX * 1 + (this.sensitivity_level * (this.max_values[behav].AX - this.mean_mu_params[behav].AX));
+      peak_threshold_values.push(threshold_value_ax);
+      var threshold_value_ay = this.mean_mu_params[behav].AY * 1 + (this.sensitivity_level * (this.max_values[behav].AY - this.mean_mu_params[behav].AY));
+      peak_threshold_values.push(threshold_value_ay);
+      var threshold_value_az = this.mean_mu_params[behav].AZ * 1 + (this.sensitivity_level * (this.max_values[behav].AZ - this.mean_mu_params[behav].AZ));
+      peak_threshold_values.push(threshold_value_az);
+      var threshold_value_gx = this.mean_mu_params[behav].GX * 1 + (this.sensitivity_level * (this.max_values[behav].GX - this.mean_mu_params[behav].GX));
+      peak_threshold_values.push(threshold_value_gx);
+      var threshold_value_gy = this.mean_mu_params[behav].GY * 1 + (this.sensitivity_level * (this.max_values[behav].GY - this.mean_mu_params[behav].GY));
+      peak_threshold_values.push(threshold_value_gy);
+      var threshold_value_gz = this.mean_mu_params[behav].GZ * 1 + (this.sensitivity_level * (this.max_values[behav].GZ - this.mean_mu_params[behav].GZ));
+      peak_threshold_values.push(threshold_value_gz);
+      var threshold_value_mx = this.mean_mu_params[behav].MX * 1 + (this.sensitivity_level * (this.max_values[behav].MX - this.mean_mu_params[behav].MX));
+      peak_threshold_values.push(threshold_value_mx);
+      var threshold_value_my = this.mean_mu_params[behav].MY * 1 + (this.sensitivity_level * (this.max_values[behav].MY - this.mean_mu_params[behav].MY));
+      peak_threshold_values.push(threshold_value_my);
+      var threshold_value_mz = this.mean_mu_params[behav].MZ * 1 + (this.sensitivity_level * (this.max_values[behav].MZ - this.mean_mu_params[behav].MZ));
+      peak_threshold_values.push(threshold_value_mz);
+
+      this.spike_threshold_params[behav] = peak_threshold_values;
+    }
+
+
     this.steps = 0;
 
     this.scale = 0.001;
@@ -59,7 +103,6 @@ export class NaiveBayesClassifier {
     this.mys = [];
     this.mzs = [];
   }
-
 
 
 
@@ -104,6 +147,31 @@ export class NaiveBayesClassifier {
     if (mx < 0) mx *= -1;
     if (my < 0) my *= -1;
     if (mz < 0) mz *= -1;
+
+    // can add timer to get rid of multiple small spikes
+    if (!this.spike_begin && ay >= this.spike_threshold_params[this.current_behavior_state][1]) {
+      if (Date.now() - this.last_spike_end_time > 500) {
+        this.spike_begin = true; // detected a possible spike
+      } else {
+        console.log("noise...")
+      }
+    } else if (this.spike_begin && ay <= this.spike_threshold_params[this.current_behavior_state][1]) {
+      this.behaviorsDetailCounts[this.current_behavior_state] += 1;
+      this.steps2 = 0;
+      this.callback({type: this.current_behavior_state});
+      // document.getElementById(current_behavior_state+"count").innerHTML = current_behavior_state + ": " + behaviorsDetailCounts[current_behavior_state];
+      this.spike_begin = false;
+      this.actually_staying_still = false; // detected the end of a spike
+
+      this.last_spike_end_time = Date.now();
+    } else if (!this.spike_begin && this.steps2 > (this.frameSize + 100) && this.current_behavior_state!="still"){
+      console.log("actually still...")
+      this.actually_staying_still = true;
+      this.behaviorsDetailCounts[this.current_behavior_state] -= 1;  // undone the wrong counting
+      this.callback({type: this.current_behavior_state});
+      this.current_behavior_state = "still";
+      this.steps2 = 0;
+    }
 
     this.axs.push(ax);
     this.ays.push(ay);
@@ -163,11 +231,22 @@ export class NaiveBayesClassifier {
         prob = prob * p1v * p2v * p3v * p4v * p5v * p6v * p7v * p8v * p9v;
         probabilities[behav] = prob;
       }
-
       let predictedBehavior = getMostLikelyBehavior(probabilities);
+      if (this.actually_staying_still) {
+        this.current_behavior_state = "still";
+      } else {
+        this.current_behavior_state = predictedBehavior;
+      }
+
       this.behaviorsCounts[predictedBehavior] += 1;
-      // console.log(predictedBehavior);
-      this.callback({type: predictedBehavior});
+      console.log(predictedBehavior + " vs. filtered: " +  this.current_behavior_state);
+      console.log(this.spike_begin + " " + this.steps2 + " " + this.current_behavior_state + " " + this.actually_staying_still)
+      // this.callback({type: predictedBehavior});
+
+      if (this.steps2 > this.frameSize + 100) {
+        this.spike_begin = false;
+      }
+
       this.steps = 0;
       this.axs = [];
       this.ays = [];
@@ -180,6 +259,7 @@ export class NaiveBayesClassifier {
       this.mzs = [];
     }
     this.steps += 1;
+    this.steps2 +=1;
   }
 }
 
